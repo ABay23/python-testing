23cbb10eaa249767db328ba126829e1fa73eb9cc
ABay23 <alejandrobay23@gmail.com>
2023-07-06 20:05:17 -0400

    First commit

---
+++ testing.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a = 33
+print(f"This is the number I was looking for {40}, but this is not the number:{50} and we can add variables too: {a}")
+
+print('Hello My friends')
+
+def caraota_blanca():
+  print('Hey this is working!')
+  
+  
+caraota_blanca()
