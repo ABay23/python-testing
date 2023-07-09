376d4059473e6d8543df88059fb746b6868b1df7
ABay23 <alejandrobay23@gmail.com>
2023-07-08 20:53:22 -0400

    New HAshmap

---
+++ hasmaps.py
@@ -0,0 +1,31 @@
+# 49. Group Anagrams
+# Medium
+# 16K
+# 453
+# Companies
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+from collections import defaultdict
+List= []
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        result = []
+
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            anagram_map[sorted_s].append(s)
+
+        for value in anagram_map.values():
+            result.append(value)
+        return result
+      
