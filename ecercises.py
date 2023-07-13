63df079f1dffad116de1e3b89f9c5939bf89e1b8
ABay23 <alejandrobay23@gmail.com>
2023-07-13 19:57:54 -0400

    New DSA folder

---
+++ ecercises.py
@@ -0,0 +1,18 @@
+
+# Division
+def reg_division(n, x):
+    res = n / x
+    print(res)
+
+
+reg_division(10, 5)
+
+# Floor division
+
+
+def floor_division(n, x):
+    res = n // x
+    print(res)
+
+
+floor_division(15, 2)
