60fa114330e0b4a9efc23a67c54181181e84a98a
ABay23 <alejandrobay23@gmail.com>
2023-07-14 00:35:41 -0400

    New DSA LL

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@
 #     self.name = name
 #     self.color = color
 #     self.weight = weight
-  
+
 #   def introduce_self(self):
 #     print("My name is " + self.name) # This is Java
 
@@ -26,35 +26,35 @@
 # r1.introduce_self()
 # r2.introduce_self()
 
-class Client:
-  increment_amount = 1.05
-  def __init__(self, name, last_name, email, open_account):
-    self.name = name
-    self.last_name = last_name
-    self.email = email
-    self.open_account = open_account
-    
-  def customer_info(self):
-    print(f'Wellcome {self.name} {self.last_name} your accound was open with ${self.open_account}')
-  
-  def increase_amount(self):
-    o_acc = int(self.open_account)
-    new_amount =int(o_acc * self.increment_amount)
-    print(f"Your New total is: ${new_amount}")
-    
-  # @classmethod
-  # def from_string(cls, client_str):
-  #   name, last_name, email, open_account = client_str.split("-")
-  #   return cls(name, last_name, email, open_account)
+# class Client:
+#   increment_amount = 1.05
+#   def __init__(self, name, last_name, email, open_account):
+#     self.name = name
+#     self.last_name = last_name
+#     self.email = email
+#     self.open_account = open_account
+
+#   def customer_info(self):
+#     print(f'Wellcome {self.name} {self.last_name} your accound was open with ${self.open_account}')
+
+#   def increase_amount(self):
+#     o_acc = int(self.open_account)
+#     new_amount =int(o_acc * self.increment_amount)
+#     print(f"Your New total is: ${new_amount}")
+
+# @classmethod
+# def from_string(cls, client_str):
+#   name, last_name, email, open_account = client_str.split("-")
+#   return cls(name, last_name, email, open_account)
 
 
 # Inheritance from Client Class
-class Developer(Client):
-  increment_amount = 10
-  
-  def __init__(self, name, last_name, email, open_account, prog_lang):
-    super().__init__(name, last_name, email, open_account)
-    self.prog_lang = prog_lang
+# class Developer(Client):
+#   increment_amount = 10
+
+#   def __init__(self, name, last_name, email, open_account, prog_lang):
+#     super().__init__(name, last_name, email, open_account)
+#     self.prog_lang = prog_lang
 
 # client_str_1 = 'Alejandro-Bay-abay@gmail.com-76000'
 # client_str_2 = 'Pedro-Taing-ptaing@gmail.com-50000'
@@ -62,7 +62,7 @@ class Developer(Client):
 # dev_str_1 = 'Alejandro-Bay-abay@gmail.com-76000'
 # dev_str_2 = 'Pedro-Taing-ptaing@gmail.com-50000'
 
-dev_str_1 = 'Alex', 'Bay', 'abay@gmail.com', 76000, 'Javascript'
+# dev_str_1 = 'Alex', 'Bay', 'abay@gmail.com', 76000, 'Javascript'
 
 # # name, last_name, email, open_account = client_str_1.split("-")
 # Client_1 = Client.from_string(client_str_2)
@@ -77,7 +77,67 @@ dev_str_1 = 'Alex', 'Bay', 'abay@gmail.com', 76000, 'Javascript'
 # Dev_1.customer_info()
 # Dev_1.increase_amount()
 
-dev_1 = Developer('Alex', 'Bay', 'abay@gmail.com', 76000, 'Javascript')
+# dev_1 = Developer('Alex', 'Bay', 'abay@gmail.com', 76000, 'Javascript')
 
-print(dev_1.customer_info())
-print(dev_1.prog_lang)
+# print(dev_1.customer_info())
+# print(dev_1.prog_lang)
+
+
+# class Cookie:
+#     def __init__(self, color):
+#         self.color = color
+
+#     def get_color(self):
+#         return self.color
+
+#     def set_color(self, color):
+#         self.color = color
+
+
+# cookie_one = Cookie('green')
+# cookie_two = Cookie('blue')
+
+# print(cookie_one.get_color())
+# print(cookie_two.get_color())
+
+
+# * Linked Lists Constructor
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+my_linked_list = LinkedList(4)
+
+print(my_linked_list.head.value)
+
+# * Linked List Print list
+
+
+def print_list(self):  # Create a method passing self
+    temp = self.head     # create a temporary variable to assign the head
+    while temp is not None:  # Loop through the head value on the list and stops when it's None = last on the list
+        print(temp.value)   # Print the .value
+    # Assign the next value to temp var - point to the next Node and run the loop again.
+        temp = temp.next
+
+# * Append new node to the end of the LL
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node  # * We change the pointer to the next node
+            self.tail = new_node  # * We change the tail to the new node
+        self.length += 1
+        return True
