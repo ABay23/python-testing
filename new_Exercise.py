5c919865f94c58b45d49ebabc188568cee28edc8
ABay23 <alejandrobay23@gmail.com>
2023-07-25 22:38:47 -0400

    New Day 3

---
+++ new_Exercise.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+# year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+year = 2400
+# Write your code below this line 👇
+print(year % 4)
+print(year % 100)
+print(year % 400)
+print(year / 4)
+print(year / 100)
+print(year / 400)
+
+
+# if year % 4 == 0 and year % 100 == 0 :
+#     print("Leap year.")
+# elif year % 100 != 0 and year % 400 == 0 :
+#     print("Leap year.")
+# else:
+#     print("Not leap year.")
