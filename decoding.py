6e44899e8a816a5fa61bfbe065e6434cbf9b400c
ABay23 <alejandrobay23@gmail.com>
2023-08-14 21:56:53 -0400

    Added 3 methods

---
+++ decoding.py
@@ -0,0 +1,7 @@
+import base64
+
+encoded_url = "aHR0cHM6Ly90bnM0bHBnbXppaXlwbnh4emVsNXNzNW55dTBuZnRvbC5sYW1iZGEtdXJsLnVzLWVhc3QtMS5vbi5hd3MvcmFtcC1jaGFsbGVuZ2UtaW5zdHJ1Y3Rpb25zLw=="
+decoded_bytes = base64.b64decode(encoded_url)
+decoded_url = decoded_bytes.decode('utf-8')
+
+print(decoded_url)
