1760ef06e3b7e1a2d73c9292669e2199770fcccd
ABay23 <alejandrobay23@gmail.com>
2023-09-02 22:18:32 -0400

    Linked Test

--- llisttest.py
+++ llisttest.py
@@ -45,10 +45,36 @@ class Linked_List:
                 self.tail = None
             return temp
 
-    def prepend(self):
-        pass
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
 
     def pop_first(self):
+        if self.length == 0:
+            return None
+
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length = - 1
+        if self.lenght == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        pass
+
+    def set_value(self, value, index):
+        pass
+
+    def insert(self, index, value):
         pass
 
 
